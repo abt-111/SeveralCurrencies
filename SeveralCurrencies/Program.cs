29f0497f173ecd5c5d4b0da9770606091640b008
abt-111 <anthony.temmel@gmail.com>
2024-07-02 20:15:34 +0200

    Ajout du code

--- SeveralCurrencies/Program.cs
+++ SeveralCurrencies/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Pour pouvoir afficher des caractères encodé en UTF8 dans la console
+        Console.OutputEncoding = Encoding.UTF8;
+
+        float money = 7.77f;
+
+        ShowMoney showMoney;
+
+        showMoney = ShowDollars;
+        Console.WriteLine(showMoney(money));
+
+        showMoney = ShowEuros;
+        Console.WriteLine(showMoney(money));
+    }
+
+    public delegate string ShowMoney(float money);
+
+    public static string ShowDollars(float money)
+    {
+        return $"{money} $";
+    }
+
+    public static string ShowEuros(float money)
+    {
+        return $"{money} €";
+    }
+}
